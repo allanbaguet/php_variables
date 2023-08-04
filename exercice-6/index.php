a2532d43a8d995c64948b6b5150c2b4e4d632fc9
Allan Baguet <allan.baguet80@gmail.com>
2023-08-04 09:15:08 +0200

    first commit

---
+++ exercice-6/index.php
@@ -0,0 +1,81 @@
+<?php
+    $name = 'Jean';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="/exercice-2/public/assets/css/style.css">
+    <title>Exercice 6</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-secondary d-flex">
+        <div class="container-fluid">
+            <a class="navbar-brand fs-2" href="#">Variables</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav fs-4">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-1/exercice1.php">Exercice 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-2/exercice2.php">Exercice 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-3/index.php">Exercice 3</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-4/index.php">Exercice 4</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-5/index.php">Exercice 5</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/exercice-6/index.php">Exercice 6</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-7/index.php">Exercice 7</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/exercice-8/index.php">Exercice 8</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center p-5">Exercice 6</h1>
+                <p class="text-center fs-4">Créer une variable <strong>name</strong> et l'initialiser avec la valeur de votre choix.
+                    Afficher : Bonjour + <strong>name</strong> + , comment vas tu ?.</p>
+            </div>
+        </div>
+    </div>
+    <hr class="border border-4 border-dark">
+    <main>
+        <div>
+            <p>
+                <?php
+                echo "Bonjour " . $name . ", comment vas tu ?";
+                ?>               
+            </p>
+            <p>
+                <?="Bonjour " . $name . ", comment vas tu ?"?>
+                <p>Bonjour <?=$name?> comment vas tu ? </p>
+                <p><?="Bonjour $name comment vas tu ?"?></p>
+            </p>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
